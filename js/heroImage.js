4ba7bd557249e220a0fb61356dcb3377634d8903
Farrukh Ayaz <farrukh.ayazz3@gmail.com>
2022-05-05 02:49:58 +0500

    fixed png added

--- js/heroImage.js
+++ js/heroImage.js
@@ -1,14 +1,14 @@
-// document.addEventListener('DOMContentLoaded', function() {
-    var img = document.getElementById('my-image');
-    img.src = '/images/undraw/round.jpg';
-    // img.src = 'https://lh3.googleusercontent.com/d/1kmREhTIU_gx5Wx47Y6Xz_9MvrZfS9C0-';
-var themeButton = document.getElementById('theme-btn')
+// // document.addEventListener('DOMContentLoaded', function() {
+//     var img = document.getElementById('my-image');
+//     img.src = '/images/undraw/round.jpg';
+//     // img.src = 'https://lh3.googleusercontent.com/d/1kmREhTIU_gx5Wx47Y6Xz_9MvrZfS9C0-';
+// var themeButton = document.getElementById('theme-btn')
 
 
-themeButton.addEventListener('click', function () {
-    console.log('imgsrc: ', img.src)
-    // img.src = img.src == 'http://127.0.0.1:5500/images/undraw/roundbw.png' ? 'https://lh3.googleusercontent.com/d/1kmREhTIU_gx5Wx47Y6Xz_9MvrZfS9C0-' : 'https://lh3.googleusercontent.com/d/1rlxPs4x1RjBSbHfdKMAW_5YCs9FxGIDB';
-    img.src = img.src.includes('.png') ? '/images/undraw/round.jpg' : '/images/undraw/roundbw.png';
+// themeButton.addEventListener('click', function () {
+//     console.log('imgsrc: ', img.src)
+//     // img.src = img.src == 'http://127.0.0.1:5500/images/undraw/roundbw.png' ? 'https://lh3.googleusercontent.com/d/1kmREhTIU_gx5Wx47Y6Xz_9MvrZfS9C0-' : 'https://lh3.googleusercontent.com/d/1rlxPs4x1RjBSbHfdKMAW_5YCs9FxGIDB';
+//     img.src = img.src.includes('.png') ? '/images/undraw/round.jpg' : '/images/undraw/roundbw.png';
 
-})
+// })
 // });
