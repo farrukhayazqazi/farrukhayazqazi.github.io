94b615027eaf936a50de5f8e4b98a8d9702ecebe
Farrukh Ayaz <farrukh.ayazz3@gmail.com>
2022-05-05 03:35:01 +0500

    should work now

--- js/heroImage.js
+++ js/heroImage.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     var themeButton = document.getElementById("theme-btn");
     themeButton.addEventListener("click", function() {
         var image = document.getElementById("my-image")
-        image.src = image.src.includes('roundbw') ? '/images/undraw/round.png' : '/images/undraw/roundbw.png'
+        image.src = image.src.includes('gx5Wx47Y6Xz_9MvrZfS9C0-') ? 'https://lh3.googleusercontent.com/d/1rlxPs4x1RjBSbHfdKMAW_5YCs9FxGIDB' : 'https://lh3.googleusercontent.com/d/1kmREhTIU_gx5Wx47Y6Xz_9MvrZfS9C0-'
     })
 })
