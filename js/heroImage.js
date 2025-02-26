1d5fdf620e8f60faf658632a38679f3da630439c
farrukhayazqazi <farrukh.ayazz3@gmail.com>
2025-02-27 00:02:17 +0500

    Update theme switch logic and implement image transitions
    Refactored the theme switch functionality to toggle between two images with a fade effect using CSS transitions. Introduced a new image container and updated HTML structure for better organization. Adjusted styling and margins to improve layout and visual consistency.

--- js/heroImage.js
+++ js/heroImage.js
@@ -1,11 +1,5 @@
-// lazyload();
-// window.onload = document.getElementById("loading").style.visibility = 'hidden';
-
 document.addEventListener("DOMContentLoaded", function() {
     var themeButton = document.getElementById("toggle-theme-btn");
-
-
-
     themeButton.addEventListener("click", function() {
         console.log('theme changed');
         console.log('themeButton.checked:: ', themeButton.checked)
@@ -16,28 +10,16 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log('image2: ', image2);
 
         if (themeButton.checked) {
-            // Fade out image1, fade in image2
             image1.classList.add("hidden");
             image2.classList.remove("hidden");
         } else {
-            // Fade out image2, fade in image1
             image2.classList.add("hidden");
             image1.classList.remove("hidden");
         }
     });
 
 
+    //    Image 1 --> 'https://lh3.googleusercontent.com/d/1rlxPs4x1RjBSbHfdKMAW_5YCs9FxGIDB'
+    //    Image 2 --> 'https://lh3.googleusercontent.com/d/1kmREhTIU_gx5Wx47Y6Xz_9MvrZfS9C0-'
 
-
-
-
-
-
-
-
-    // themeButton.addEventListener("click", function() {
-    //     console.log('theme changed');
-    //     var image = document.getElementById("my-image")
-    //     image.src = themeButton.checked ? 'https://lh3.googleusercontent.com/d/1rlxPs4x1RjBSbHfdKMAW_5YCs9FxGIDB' : 'https://lh3.googleusercontent.com/d/1kmREhTIU_gx5Wx47Y6Xz_9MvrZfS9C0-'
-    // })
 })
