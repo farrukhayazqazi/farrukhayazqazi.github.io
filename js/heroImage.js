758127351609c8f5b44ef0150a486dd342c6fdae
farrukhayazqazi <farrukh.ayazz3@gmail.com>
2025-02-26 23:40:19 +0500

    Update theme switch logic and implement image transitions
    Refactored the theme switch functionality to toggle between two images with a fade effect using CSS transitions. Introduced a new image container and updated HTML structure for better organization. Adjusted styling and margins to improve layout and visual consistency.

--- js/heroImage.js
+++ js/heroImage.js
@@ -1,10 +1,43 @@
-lazyload();
-window.onload = document.getElementById("loading").style.visibility = 'hidden';
+// lazyload();
+// window.onload = document.getElementById("loading").style.visibility = 'hidden';
 
 document.addEventListener("DOMContentLoaded", function() {
-    var themeButton = document.getElementById("theme-btn");
+    var themeButton = document.getElementById("toggle-theme-btn");
+
+
+
     themeButton.addEventListener("click", function() {
-        var image = document.getElementById("my-image")
-        image.src = image.src.includes('gx5Wx47Y6Xz_9MvrZfS9C0-') ? 'https://lh3.googleusercontent.com/d/1rlxPs4x1RjBSbHfdKMAW_5YCs9FxGIDB' : 'https://lh3.googleusercontent.com/d/1kmREhTIU_gx5Wx47Y6Xz_9MvrZfS9C0-'
-    })
+        console.log('theme changed');
+        console.log('themeButton.checked:: ', themeButton.checked)
+
+        const image1 = document.getElementById("image-1");
+        console.log('image1; ', image1);
+        const image2 = document.getElementById("image-2");
+        console.log('image2: ', image2);
+
+        if (themeButton.checked) {
+            // Fade out image1, fade in image2
+            image1.classList.add("hidden");
+            image2.classList.remove("hidden");
+        } else {
+            // Fade out image2, fade in image1
+            image2.classList.add("hidden");
+            image1.classList.remove("hidden");
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+    // themeButton.addEventListener("click", function() {
+    //     console.log('theme changed');
+    //     var image = document.getElementById("my-image")
+    //     image.src = themeButton.checked ? 'https://lh3.googleusercontent.com/d/1rlxPs4x1RjBSbHfdKMAW_5YCs9FxGIDB' : 'https://lh3.googleusercontent.com/d/1kmREhTIU_gx5Wx47Y6Xz_9MvrZfS9C0-'
+    // })
 })
